f7f471356382972e83cf447a78e8d84b7a90c849
fillipp <fil.mikhayloch@mail.ru>
2021-11-12 16:43:57 +0300

    добавил базу данны

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
+
+from webapp.model import db, News
 from webapp.weather import weather_by_city
-from webapp.news import get_python_news
+
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
 
     @app.route("/")
     def hindex():
         tatle = "Новосто Python"
-        weather = weather_by_city("Moscow,Russia")
-        news_list = get_python_news()
+        weather = weather_by_city(app.config["WEATHER_DEFAULT_CITY"])
+        news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_tatle = tatle, weather=weather, news_list = news_list)
     return app
